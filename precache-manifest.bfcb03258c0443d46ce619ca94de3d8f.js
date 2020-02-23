96d2610f21f9f450248a0511fcbd22bb59a15cbc
Marcus Davis <mwayn316@gmail.com>
2020-02-23 09:49:02 -0600

    First Commit

---
+++ precache-manifest.bfcb03258c0443d46ce619ca94de3d8f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d34a2be05737798badcc0399e196348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e400481007c28270ac",
+    "url": "/static/css/main.94982eb6.chunk.css"
+  },
+  {
+    "revision": "a12e530c6d1664b4526b",
+    "url": "/static/js/2.bca7a77b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bca7a77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e400481007c28270ac",
+    "url": "/static/js/main.603e1436.chunk.js"
+  },
+  {
+    "revision": "d89b2078b5719f7a2fe0",
+    "url": "/static/js/runtime-main.4cc1305f.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/static/media/image-rules.5d74d25d.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/static/media/logo.759a2075.svg"
+  }
+]);
